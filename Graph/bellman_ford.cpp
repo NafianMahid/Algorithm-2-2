6741f9bee068bf4e37055ea9d26de6ff4cf7a2f1
Nafian-23 <nafiannazatmahid@Nafians-MacBook-Air.local>
2026-05-17 23:04:02 +0600

    bellman ford algorithm

---
+++ Graph/bellman_ford.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int v,e;
+  cin >> v >> e;
+  vector<vector<int>>edges(e);
+  vector<int>dist(v,1e8);
+  dist[0] = 0;
+  for (int i = 0; i < e; i++)
+  {
+    int u,v,w;
+    cin >> u >> v >> w;
+    edges[i] = {u,v,w};
+  }
+  
+  for (int i = 0; i < v-1; i++)
+  {
+    bool flag = 0;
+    // Relax all the edges
+    for (int j = 0; j < e; j++)
+    {
+      int u = edges[j][0];
+      int v = edges[j][1];
+      int w = edges[j][2];
+      if(dist[u]== 1e8){
+        continue;
+      }
+      if(dist[u] + w < dist[v]){
+        dist[v] = dist[u] + w;
+        flag = 1;
+      }
+    }
+    if(flag == 0){
+      break;
+    }
+  }
+  //To deduct the cycle
+  for (int j = 0; j < e; j++)
+    {
+      int u = edges[j][0];
+      int v = edges[j][1];
+      int w = edges[j][2];
+      if(dist[u]== 1e8){
+        continue;
+      }
+      if(dist[u] + w < dist[v]){
+        cout << -1 << endl;
+        return;
+      }
+    }
+    for (int i = 0; i < v; i++)
+    {
+      cout << dist[i] <<" ";
+    }
+}
+
+// time complexity: (v-1)* e + e => VE;
+// best case : E (inner loop runs only e time once)
+// space complexity : V
